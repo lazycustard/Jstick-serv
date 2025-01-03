f8b0caf5a0cb470f409b4a3cee9b0a64af009dbf
Bani Gupta <bani.gu2007@gmail.com>
2025-01-04 00:38:13 +0530

    Update p1-joystick-servo-control.cpp

--- p1-joystick-servo-control.cpp
+++ p1-joystick-servo-control.cpp
@@ -1,14 +1,22 @@
-#include<servo.h>
+#include <Servo.h>
+
 #define Servo_Pin 2
 Servo myServo;
 
-void setup(){
+void setup() {
+    Serial.begin(9600);          
+    myServo.attach(Servo_Pin);    
+}
 
-myServo.attach(Servo_Pin); 
-void loop()
-{
- int joystickValue = analogRead(A0);
- int output = map(joystickValue, 0, 1023, 0, 180);
- myServo.write(output);
- delay(20);
+void loop() {
+    int joystickValue = analogRead(A0);  
+    int output = map(joystickValue, 0, 1023, 0, 180);  /
+    
+    Serial.print("Joystick Value: ");
+    Serial.print(joystickValue);
+    Serial.print("\t Servo Output: ");
+    Serial.println(output);
+    
+    myServo.write(output);  
+    delay(20);  /
 }
